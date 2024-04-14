14f88ae26fa5ad5381b994b2683d279f4a274d41
Vishnu Vardhan <43066996+me-so-cool@users.noreply.github.com>
2024-04-14 11:29:41 +0100

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"Newcastle#4321",
+    password:"qwerty#1234",
     database:"blog",
 }
 )
